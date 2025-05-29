a2ef8014c9cca329237f79c3fd3b4d1343bc6d8e
Vinay <vinaykgupta04@gmail.com>
2025-05-29 18:01:40 +0530

    Index.js update register route

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,10 @@
 const express = require('express');
 require('./db/config');
+const User = require('./db/User');
 const app = express();
+
+app.post("/register",(req,resp)=>{
+    resp.send("Api is working now...........")
+});
 
 app.listen(5000)
